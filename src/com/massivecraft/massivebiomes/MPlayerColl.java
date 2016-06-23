6cf20ab256aaf5731174ce88e13e6a866a38bdeb
Olof Larsson <olof@sylt.nu>
2016-06-23 11:55:00 +0200

    MassiveCore - Automatic Collection Constructors

--- src/com/massivecraft/massivebiomes/MPlayerColl.java
+++ src/com/massivecraft/massivebiomes/MPlayerColl.java
@@ -1,6 +1,5 @@
 package com.massivecraft.massivebiomes;
 
-import com.massivecraft.massivecore.store.MStore;
 import com.massivecraft.massivecore.store.SenderColl;
 
 public class MPlayerColl extends SenderColl<MPlayer>
@@ -11,10 +10,6 @@ public class MPlayerColl extends SenderColl<MPlayer>
 	
 	private static MPlayerColl i = new MPlayerColl();
 	public static MPlayerColl get() { return i; }
-	private MPlayerColl()
-	{
-		super("massivebiomes_mplayer", MPlayer.class, MStore.getDb(), MassiveBiomes.get());
-	}
 
 	// -------------------------------------------- //
 	// STACK TRACEABILITY
